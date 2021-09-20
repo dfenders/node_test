062399d84d9b8c5732a95f7aa8658c97ea823b55
Adon Moskal <adonm@op.ac.nz>
2021-09-20 17:00:15 +1200

    ...

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send("Hello world"))
-app.get('/contact', (req, res) => res.send("Hello contact"))
+app.set("view engine","ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressLayouts)
+app.use(express.static(path.join(__dirname, 'assets')))
+
+app.get('/', (req, res) => {    
+    let user = {"name":"Adon"};
+    res.render('index',user)
+})
+app.get('/about', (req, res) => res.render("about"))
 app.get('/user', (req, res) => res.send("Hello user"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
