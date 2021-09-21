f9cbec59063c7d76497cc789a1107e0c8d0c5d09
Adon Moskal <adonm@op.ac.nz>
2021-09-21 14:31:02 +1200

    ...

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
 const port = 3000
 
+
 app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(expressLayouts)
 app.use(express.static(path.join(__dirname, 'assets')))
+app.use(expressLayouts)
 
-app.get('/', (req, res) => {    
-    let user = {"name":"Adon"};
+
+app.get('/', (req, res) => {   
+    //if this user is logged in 
+    let user = {"name":"Mike","title":"Index"};
     res.render('index',user)
 })
-app.get('/about', (req, res) => res.render("about"))
+app.get('/about', (req, res) => res.render("about",{"title":"about"}))
 app.get('/user', (req, res) => res.send("Hello user"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
